a7557a57cfb0e3ca6afa7d02b7947bd38f565e12
Tech-vibe <makerapp76@gmail.com>
2025-03-25 01:09:03 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{html,js}',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          pacifico: ['pacifico', 'cursive'],
+        },
+      },
+    },
+    plugins: [],
+}
+  
+  
